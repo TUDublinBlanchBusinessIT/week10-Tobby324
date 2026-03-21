794f20c4c9128f49a34e1915b172f06782e6ea35
Tobby Akinwale <B00165442@mytudublin.ie>
2026-03-21 08:12:19 +0000

    Blog 9 - Enforcing roles and permissions with middleware

--- routes/web.php
+++ routes/web.php
@@ -31,11 +31,22 @@ Route::resource('bookings', App\Http\Controllers\bookingController::class);
 
 Route::get('/loggedInMember','App\Http\Controllers\memberController@getLoggedInMemberDetails');
 
-Route::resource('users', App\Http\Controllers\usersController::class);
+Route::resource('courts', App\Http\Controllers\courtController::class);
 
-Route::resource('roles', App\Http\Controllers\rolesController::class);
+Route::delete('/bookings/{booking}', 'App\Http\Controllers\bookingController@destroy')->name('bookings.destroy')->middleware('permission:Delete Booking');
 
-Route::resource('permissions', App\Http\Controllers\permissionsController::class);
+Route::delete('/members/{member}', 'App\Http\Controllers\memberController@destroy')->name('members.destroy')->middleware('permission:Delete Member');
+
+Route::group(['middleware' => ['permission:Create New Member']], function () {
+    Route::get('/members/create', 'App\Http\Controllers\memberController@create')->name('members.create');
+    Route::post('/members/store', 'App\Http\Controllers\memberController@store')->name('members.store');
+});
+
+Route::group(['middleware' => ['role:System Admin']], function () {
+    Route::resource('roles', App\Http\Controllers\rolesController::class);
+    Route::resource('permissions', App\Http\Controllers\permissionsController::class);
+    Route::resource('users', App\Http\Controllers\usersController::class);
+});
 
 Route::get('/users/assignroles/{id}', 'App\Http\Controllers\usersController@assignRoles')->name('users.assignroles');
 
